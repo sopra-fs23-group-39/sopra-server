f6271b605e6e47bb85d3fbc3ee9836a2469cc345
NieMark <126515593+NieMark@users.noreply.github.com>
2023-04-25 15:40:20 +0200

    Revert "Revert "Revert "Changed naming conventions to totalPointsCurrentGame"""
    This reverts commit ce1f60befd56dbee7630d4f609b0732629ed1a8d.

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserGetDTO.java
@@ -17,7 +17,7 @@ public class UserGetDTO {
 
     private Long numberGames;
 
-    private Long totalPointsCurrentGame;
+    private Long totalPoints;
 
     private Long currentPoints;
 
@@ -96,12 +96,12 @@ public class UserGetDTO {
         this.numberGames = numberGames;
     }
 
-    public Long getTotalPointsCurrentGame() {
-        return totalPointsCurrentGame;
+    public Long getTotalPoints() {
+        return totalPoints;
     }
 
-    public void setTotalPointsCurrentGame(Long totalPointsCurrentGame) {
-        this.totalPointsCurrentGame = totalPointsCurrentGame;
+    public void setTotalPoints(Long totalPoints) {
+        this.totalPoints = totalPoints;
     }
 
   public UserStatus getStatus() {
