470817eb4aa80d9f67d4e3eccdad4f336c94d60b
NieMark <126515593+NieMark@users.noreply.github.com>
2023-05-24 20:37:36 +0200

    Fixed only 2 players being able to play the game at maximum

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketController.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.websocket;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import ch.uzh.ifi.hase.soprafs23.constant.GameFormat;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
@@ -20,6 +21,8 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 
 @Controller
@@ -30,7 +33,10 @@ public class WebSocketController {
     private final UserService userService;
     //private final Map<Long, ArrayList<WebSocketSession>> hostSessions = new HashMap<>();
     private final Map<Long, Question> currentGameQuestions = new HashMap<>();
-    private final Map<Long, String> connectedClients = new HashMap<>();
+    //private final Map<Long, String> connectedClients = new ConcurrentHashMap<>();
+    private final Multimap<Long, String> multiConnectedClients = ArrayListMultimap.create();
+    private final Map<String, Long> reverseConnectedClients = new ConcurrentHashMap<>();
+
 
     public WebSocketController(SimpMessagingTemplate messagingTemplate, GameService gameService, UserService userService) {
         this.messagingTemplate = messagingTemplate;
@@ -81,6 +87,7 @@ public class WebSocketController {
     public QuestionGetDTO sendQuestion(@DestinationVariable Long gameId, SimpMessageHeaderAccessor headerAccessor) {
 
         String sessionId = headerAccessor.getSessionId();
+        System.out.println("New connection from " + sessionId);
         /*synchronized (connectedClients) {
             if(connectedClients.isEmpty()){
                 Question question = gameService.getQuestionToSend(gameId);
@@ -95,31 +102,52 @@ public class WebSocketController {
                 return DTOMapper.INSTANCE.convertEntityToQuestionGetDTO(currentGameQuestions.get(gameId));
             }
         }*/
-        synchronized (connectedClients) {
-            if(connectedClients.size() != gameService.getHostAndPlayers(gameId).size()-1){
-                /*Question question = gameService.getQuestionToSend(gameId);
-                Date creationTime = new Date();
-                question.setCreationTime(creationTime);
-                currentGameQuestions.put(gameId, question);*/
+
+        //connectedClients.put(gameId, sessionId);
+
+        //System.out.println(connectedClients.size());
+        //System.out.println("clients connected: " + connectedClients.get(gameId));
+        System.out.println("Current multi conn: " + multiConnectedClients.get(gameId).size());
+
+
+        synchronized (multiConnectedClients){
+            if(multiConnectedClients.get(gameId).size() != gameService.getHostAndPlayers(gameId).size()-1){
+                System.out.println("currently multi size: " + multiConnectedClients.get(gameId).size());
+                System.out.println("game size: " + gameService.getHostAndPlayers(gameId).size());
+            /*Question question = gameService.getQuestionToSend(gameId);
+            Date creationTime = new Date();
+            question.setCreationTime(creationTime);
+            currentGameQuestions.put(gameId, question);*/
+                //System.out.println("connected client size: " + connectedClients.size());
                 this.messagingTemplate.convertAndSend("/topic/game/" + gameId + "/question", "Waiting for players...");
-                connectedClients.put(gameId, sessionId);
+                multiConnectedClients.put(gameId, sessionId);
+                reverseConnectedClients.put(sessionId, gameId);
+
                 //return DTOMapper.INSTANCE.convertEntityToQuestionGetDTO(question);
-            }else{
+            } else {
+                System.out.println("are we in here?");
                 Question question = gameService.getQuestionToSend(gameId);
                 Date creationTime = new Date();
                 question.setCreationTime(creationTime);
                 currentGameQuestions.put(gameId, question);
-                connectedClients.put(gameId, sessionId);
+                multiConnectedClients.put(gameId, sessionId);
+                reverseConnectedClients.put(sessionId, gameId);
                 return DTOMapper.INSTANCE.convertEntityToQuestionGetDTO(currentGameQuestions.get(gameId));
             }
         }
+
+
         //unhandled so far, but works since checks are done in the front end, will think about something for this.
         return null;
     }
     @EventListener
     public void handleQuestionDisconnect(SessionDisconnectEvent event){
         String sessionId = event.getSessionId();
-        connectedClients.values().removeIf(id -> id.equals(sessionId));
+        Long key = reverseConnectedClients.get(sessionId);
+        System.out.println("key: " + key + " Session: " + reverseConnectedClients.get(sessionId) );
+        //connectedClients.values().removeIf(id -> id.equals(sessionId));
+        multiConnectedClients.get(key).remove(sessionId);
+        reverseConnectedClients.remove(sessionId);
     }
 
     @MessageMapping("/game/{gameId}/question/non-host")
